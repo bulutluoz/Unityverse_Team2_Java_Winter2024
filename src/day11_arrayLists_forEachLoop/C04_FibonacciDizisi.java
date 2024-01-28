307d35757c5c23913a1838db1da387ec2243caff
bulutluoz <abbulutluoz@gmail.com>
2024-01-28 16:49:41 +0100

    11.gun sonu

---
+++ src/day11_arrayLists_forEachLoop/C04_FibonacciDizisi.java
@@ -0,0 +1,25 @@
+package day11_arrayLists_forEachLoop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C04_FibonacciDizisi {
+
+    public static void main(String[] args) {
+
+        //Soru 4- Verilen pozitif bir n tamsayisini alarak,
+        //        bize ilk n tane tane Fibonacci sayisini bir list olarak donduren
+        //        bir method olusturun.
+        //        Fibonacci Dizisi : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,
+
+
+    }
+
+    public static List<Integer> ilkNtaneFibonacciSayisi(int sayiAdedi){
+
+        List<Integer> fibonacciSerisi = new ArrayList<>();
+
+
+        return fibonacciSerisi;
+    }
+}
