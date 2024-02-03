adecd2b422bdc74083a971ad3c566d2ee48bf68d
bulutluoz <abbulutluoz@gmail.com>
2024-02-03 18:49:52 +0100

    day12 sonu

---
+++ src/day12_constructors/C06_ConstructorCall.java
@@ -0,0 +1,44 @@
+package day12_constructors;
+
+public class C06_ConstructorCall {
+
+    C06_ConstructorCall(){
+
+        // C06_ConstructorCall(); // Method call expected
+        // isim() seklinde yazim method call icin kullanildigindan
+        // constructorlar icin farkli bir kullanim olusturulmustur.
+
+        // constructor call icin this(parametreler)
+
+        this("Ali"); // bu class'da string parametreli constructor calissin
+        System.out.println("Parametresiz constructor calisti");
+    }
+
+    C06_ConstructorCall(String str){
+        System.out.println("Parametreli constructor calisti");
+    }
+
+
+    public static void main(String[] args) {
+
+        // method1();
+
+        C06_ConstructorCall obj = new C06_ConstructorCall();
+
+    }
+
+
+    public static void method1(){
+        methdod2();
+        System.out.println("method1 calisti");
+
+    }
+
+    public static void methdod2(){
+        System.out.println("method2 calisti");
+    }
+
+    void C06_ConstructorCall(String kelime){
+
+    }
+}
