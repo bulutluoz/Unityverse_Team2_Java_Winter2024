b12236d76704eca368027af79b3424c2c837f3e8
bulutluoz <abbulutluoz@gmail.com>
2024-02-24 18:53:19 +0100

    day18 sonu

--- src/day18_inheritance_pollyMorphism/C04_OverridingParent.java
+++ src/day18_inheritance_pollyMorphism/C04_OverridingParent.java
@@ -18,14 +18,27 @@ public class C04_OverridingParent {
 
   }
 
- public void method3(){
+
+
+
+   void method3(){
 
  }
 
 
+ public static void method4(){
+
+ }
+
+ private void method5(){
+
+ }
 
 
+ public Object method6(){
 
+        return 0;
+ }
 
 
 
--- src/day18_inheritance_pollyMorphism/C05_OverridingChild.java
+++ src/day18_inheritance_pollyMorphism/C05_OverridingChild.java
@@ -34,7 +34,7 @@ public class C05_OverridingChild extends C04_OverridingParent{
     }
 
 
-     public void method3(){
+    public void method3(){
         /*
             Her ne kadar signature dahil olmasa da
             ovverriding icin access modifier da onemlidir
@@ -42,6 +42,59 @@ public class C05_OverridingChild extends C04_OverridingParent{
             Child class'daki overriding method'un access modifier'i
             parent class'daki overridden method'un access modifier'indan
             daha kisitlayici olamaz
+
+            child parent'i kisitlayamaz
          */
     }
+
+    // private, static ve final method'lar override edilemez
+
+    public static void method4(){
+        // eger parent class'daki method final yapilirsa
+        // Java "overridden method is final" der ve CTE verir
+
+        // eger parent class'daki method static yapilirsa
+        //  "cannot override static method" der ve CTE verir
+        // eger child class'daki method'u da static yaparsak
+        // CTE olmaz ama Override da olmaz
+        // bu iki method, birbirinden bagimsiz iki method olarak kullanilabilir
+
+    }
+    public void method5(){
+       // parent class'da private bir method oldugunda
+       // child class onu goremeyeceginden
+       // method5 bagimsiz bir method gibi davranir
+       // override etmis olmaz
+    }
+
+    public String method6(){
+       // Parent class'daki method'un return type'i
+       // void veya primitive ise child class'daki method'un return type'i
+       // parent'daki method ile ayni olmalidir
+
+
+       // overridden method'un return type'i non-primitive ise
+       // child class'daki method'un return type'i
+       // Ya aynisi olmali,
+       // Veya parent'daki return type'in child'i olmali
+       // Buna Covariant denir
+
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
